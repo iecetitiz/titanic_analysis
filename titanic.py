c5478e992ad01e1c9ad60c7e5f41a097f799061e
iecetitiz <i.ecetitiz@gmail.com>
2023-05-29 17:17:10 +0300

    test and data files are added

---
+++ titanic.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 29 17:05:00 2023
+
+@author: iecet
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
